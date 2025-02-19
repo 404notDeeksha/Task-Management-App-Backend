ebdff8badd7338d675a3eae57f204730e18e891c
Deeksha_GitFiles <sharmadeekksha@gmail.com>
2025-02-19 23:39:01 +0530

    commented unused api calls

--- controllers/Task.controller.js
+++ controllers/Task.controller.js
@@ -4,7 +4,6 @@ const Task = require("../models/Task.model");
 const createTask = async (req, res) => {
   try {
     const task = new Task(req.body);
-    // console.log("Task", task);
 
     const newTask = await task.save();
     res.status(201).json({ success: true, data: newTask });
@@ -24,72 +23,39 @@ const getAllTasks = async (req, res) => {
 };
 
 // GET/api/tasks/:id
-const getTask = async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id);
-    if (!task)
-      return res
-        .status(404)
-        .json({ success: false, message: "Task not found" });
-    res.json({ success: true, data: task });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
+// const getTask = async (req, res) => {
+//   try {
+//     const task = await Task.findById(req.params.id);
+//     if (!task)
+//       return res
+//         .status(404)
+//         .json({ success: false, message: "Task not found" });
+//     res.json({ success: true, data: task });
+//   } catch (err) {
+//     res.status(500).json({ success: false, message: err.message });
+//   }
+// };
 
-// GET/api/tasks/:date
-const getTasksOfDay = async (req, res) => {
-  const date = req.params.date;
+// // GET/api/tasks/:date
+// const getTasksOfDay = async (req, res) => {
+//   const date = req.params.date;
 
-  const startOfDay = new Date(`${date}T00:00:00.000Z`);
-  const endOfDay = new Date(`${date}T23:59:59.999Z`);
+//   const startOfDay = new Date(`${date}T00:00:00.000Z`);
+//   const endOfDay = new Date(`${date}T23:59:59.999Z`);
 
-  try {
-    const tasks = await Task.find({
-      timestamp: { $gte: startOfDay, $lt: endOfDay },
-    });
+//   try {
+//     const tasks = await Task.find({
+//       timestamp: { $gte: startOfDay, $lt: endOfDay },
+//     });
 
-    res.json({ success: true, data: tasks });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
-
-// GET/api/tasks/:priority
-const getTasksPriorityWise = async (req, res) => {
-  const taskPriority = req.params.priority;
-
-  try {
-    const tasks = await Task.find({ priority: taskPriority });
-    res.json({ success: true, data: tasks });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
-};
-
-// GET/api/tasks/pending-before-today
-const getTasksPendingBeforeToday = async (req, res) => {
-  try {
-    // Get today's date at 00:00:00 (start of the day)
-    const todayStart = new Date().setHours(0, 0, 0, 0);
-
-    // Find tasks where status is "In Progress" or "To Do" and dueDate is before today
-    const tasks = await Task.find({
-      status: { $in: ["In Progress", "To Do"] },
-      dueDate: { $lt: todayStart },
-    });
-
-    res.status(200).json(tasks);
-  } catch (error) {
-    console.error("Error fetching tasks:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-};
+//     res.json({ success: true, data: tasks });
+//   } catch (err) {
+//     res.status(500).json({ success: false, message: err.message });
+//   }
+// };
 
 // Update a task // PUT/api/tasks/:id
 const updateTask = async (req, res) => {
-  // console.log("Update", req.params.id, req.body);
-
   try {
     const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -107,8 +73,6 @@ const updateTask = async (req, res) => {
 // Delete a task // DELETE/api/tasks/:id
 const deleteTask = async (req, res) => {
   try {
-    // console.log("Task", req.params.id, typeof req.params.id);
-
     const task = await Task.findByIdAndDelete(req.params.id);
     // console.log("Task", task);
     if (!task)
@@ -126,8 +90,6 @@ module.exports = {
   updateTask,
   deleteTask,
   getAllTasks,
-  getTask,
-  getTasksOfDay,
-  getTasksPriorityWise,
-  getTasksPendingBeforeToday,
 };
+// getTask,
+// getTasksOfDay,
