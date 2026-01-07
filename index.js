8a095666d6547c693f845728290cc01ec4e548f8
Deeksha <sharmadeekksha@gmail.com>
2026-01-07 21:13:24 +0530

    debug: testing mode of code environment

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const dbConnection = require("./config/dbConnection");
 const app = require("./app");
 
 if (process.env.NODE_ENV !== "test") {
-  console.log(`🟢 Starting server`);
+  // if (process.env.NODE_ENV === "development") {
+  console.log(`🟢 Starting server`, process.env.NODE_ENV);
+  // }
   dbConnection();
 
   const port = env.PORT;
-  
+
   app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
   });
